46d4e069bd3f01fd9861c2fa8dceb475d06e5af0
JacobLinCool <jacoblincool@gmail.com>
2022-10-07 13:46:23 +0800

    chore: add file store to example

--- example/index.ts
+++ example/index.ts
@@ -1,11 +1,17 @@
 import { Bot } from "pure-cat";
+import { FileStore } from "pure-cat-store-file";
 import { Ignore } from "pure-cat-module-ignore";
 import { I18n } from "pure-cat-module-i18n";
 import { Suggestion } from "../src";
 
 new Bot()
+    .use(new FileStore())
     .use(new Ignore())
     .use(new I18n())
-    .use(new Suggestion())
+    .use(
+        new Suggestion({
+            custom_list_max_rule_size: 100,
+        }),
+    )
     .start()
     .then(() => console.log("Bot started!"));
